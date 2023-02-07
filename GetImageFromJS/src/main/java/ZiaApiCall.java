4c91db10d16538fc259e9627befd246c559cc27f
AkaashOruvan <113968065+AkaashOruvan@users.noreply.github.com>
2023-02-07 23:04:56 +0530

    Update ZiaApiCall.java
    not going to use it. But save it any how for future use

--- GetImageFromJS/src/main/java/ZiaApiCall.java
+++ GetImageFromJS/src/main/java/ZiaApiCall.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
 import javax.servlet.http.Part;
@@ -43,12 +45,10 @@ public class ZiaApiCall {
 
         @Override
         public void run() {
-            if(ResponseString!=null){
-                Thread.interrupted();
-            }
+         
             HttpClient httpclient = new DefaultHttpClient();
             HttpPost httpPost = new HttpPost("https://dl.zoho.com/cv/face/compare");
-            httpPost.setHeader("Authorization","Bearer "+"1000.03efc0a5f767ae5d4fa9e80e81a05b6d.7ba8417aea12d6791ee555e47adc2155"
+            httpPost.setHeader("Authorization","Bearer "+"1000.d7e9b712f3667dc43d8a11dc29063db5.2f4ff8df9e176cc74b45267b7d8e72a2"
             		+ "");
             FileBody query_image = new FileBody(new File(queryImage));
             FileBody source_image =new FileBody(new File(source));
@@ -57,34 +57,37 @@ public class ZiaApiCall {
             reqEntity.addPart("source_image",source_image);
             httpPost.setEntity(reqEntity);
             HttpResponse response = null;
-            try {
-                response = httpclient.execute(httpPost);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
             String ap= null;
             try {
+                response = httpclient.execute(httpPost);
+           
+     
+         
                 ap = EntityUtils.toString(response.getEntity());
+                System.out.println(ap);
+            }
 //                System.out.println(ap);
-            } catch (IOException e) {
+            catch (Exception e) {
                 throw new RuntimeException(e);
             }
             JsonResponseGenerator obj=new JsonResponseGenerator();
-            if(faceNotFound) {
-            	if(obj.faceNotFound(ap)) {
-            		while(ResponseLevel!=0){
-	                    latch.countDown();
-	                    ResponseLevel--;
-	                  }
-            	}
-            	else {
-            		faceNotFound=false;
-            	}
-            }
+//            if(faceNotFound) {
+//            	if(obj.faceNotFound(ap)) {
+//            		while(ResponseLevel!=0){
+//	                    latch.countDown();
+//	                    ResponseLevel--;
+//	                  }
+//            	}
+//            	else {
+//            		faceNotFound=false;
+//            	}
+//            }
             double conf_level=(obj.getConfidence(ap));
+            System.out.println(conf_level);
             if(conf_level>0.3){
                 ResponseString=this.source;
                 Conf_Level=conf_level;
+                System.out.println(ResponseLevel);
                 while(ResponseLevel!=0){
                     latch.countDown();
                     ResponseLevel--;
@@ -106,39 +109,55 @@ public class ZiaApiCall {
 	
 	
 	
-	public String responseMethod(Collection<Part> parts) {
+	
+	
+	public String responseMethod(Collection<Part> parts) throws InterruptedException {
+		
+
 		CountDownLatch latch = new CountDownLatch(parts.size());
+
+		List<String> arr = new ArrayList<String>(); 
+		
 		for (Part part : parts) {
+	
 		    if ((!part.getName().equals("file"))) {
 		        continue;
 		    }
 		    if(part.getName().equals("file") && part.getContentType().startsWith("image/") ) {
-			    String fileName = "/home/ashwanth/picsave/"+ File.separator+getFileName(part);
+			    String fileName = "/home/ashwanth/picsave/"+ getFileName(part);
+
 			    if((fileName.endsWith(".png"))|| (fileName.endsWith(".jpg")) || (fileName.endsWith(".jpeg"))){
-			    System.out.println(fileName);
+
 			    try {
 					part.write(fileName);
-					new WorkerThread(latch,fileName).run();
+					System.out.println("Thread Started");
+					arr.add(fileName);				
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					System.out.println("Query File not saved");
-					return "{\"Message\": \"Error\"}";
-//					e.printStackTrace();
-				}	
-			    }
+
+//				}	
+//			    }
 		    }
 		}
-		try {
-			latch.await();
-		} catch (InterruptedException e) {
-			System.out.println("Latch issue InterruptedException");
-			return WorkerThread.errorMessage;
+		    }}
+		System.out.println("WOrkdone");
+		for(int i=0;i<arr.size();i++) {
+			new WorkerThread(latch,arr.get(i)).start();
 		}
+		
+		
+//		try {
+		latch.await();
+//		} catch (InterruptedException e) {
+//			System.out.println("Latch issue InterruptedException");
+//			return WorkerThread.errorMessage;
+//		}
 		String ap=WorkerThread.ResponseString;
 //        System.out.println("All worker threads have finished.");
         System.out.println(ap);
 //        JSONObject main = new JSONObject();
-        try {
+//        try {
         JSONObject details=new JSONObject(); 
         JSONObject main = new JSONObject();
         if(ap!=null) {
@@ -152,15 +171,15 @@ public class ZiaApiCall {
         main.put("Info", details);
       
         main.put("Message","Success");
-        return main.toString();
-        }
-        catch (Exception e) {
-        	return WorkerThread.errorMessage;
-        }
+        return main.toString();}
+//        }
+//        catch (Exception e) {
+//        	return WorkerThread.errorMessage;
+//        }
         
 		
 		
-	}
+	
 	private static String getFileName(Part part) {
 	    for (String contentDisposition : part.getHeader("content-disposition").split(";")) {
 	        if (contentDisposition.trim().startsWith("filename")) {
@@ -171,31 +190,31 @@ public class ZiaApiCall {
 	}
 
 }
-class JsonResponseGenerator {
-//	JSONObject jsonObject; 
-    public double getConfidence(String json) {
-    	JsonParser parser = new JsonParser();
-        JsonObject jsonObject = parser.parse(json).getAsJsonObject();
-        JsonObject data = jsonObject.getAsJsonObject("data");
-        JsonObject pairs = data.getAsJsonArray("pairs").get(0).getAsJsonObject();
-
-        double confidence = pairs.get("confidence").getAsDouble();
-       return confidence;
-    }
-    public boolean faceNotFound(String json) {
-    	String jsonString = "{\"data\": {\"query_image\": {\"0\": {\"faceCoordinate\": [\"0.315\", \"0.241\", \"0.715\", \"0.796\"]}}, \"source_image\": {\"0\": {\"faceCoordinate\": [\"0.333\", \"0.132\", \"0.778\", \"0.750\"]}}, \"pairs\": [{\"query_image\": \"0\", \"source_image\": \"0\", \"confidence\": \"-0.0759\"}]}, \"numFacesFoundQuery\": 1, \"numFacesFoundSource\": 1, \"executionMessage\": \"Faces found\", \"message\": \"success\"}";
-        JsonParser parser = new JsonParser();
-        JsonObject root = parser.parse(json).getAsJsonObject();
-        int numFacesFoundQuery = root.get("numFacesFoundQuery").getAsInt();
-        if(numFacesFoundQuery==0) {
-        	return true;
-        }
-//        System.out.println("numFacesFoundQuery: " + numFacesFoundQuery);
-    	
-    	return false;
-    	
-    	
-    }
-    
-}
-
+//class JsonResponseGenerator {
+//
+//    public double getConfidence(String json) {
+//    	JsonParser parser = new JsonParser();
+//        JsonObject jsonObject = parser.parse(json).getAsJsonObject();
+//        JsonObject data = jsonObject.getAsJsonObject("data");
+//        JsonObject pairs = data.getAsJsonArray("pairs").get(0).getAsJsonObject();
+//
+//        double confidence = pairs.get("confidence").getAsDouble();
+//       return confidence;
+//    }
+//    public boolean faceNotFound(String json) {
+//    	String jsonString = "{\"data\": {\"query_image\": {\"0\": {\"faceCoordinate\": [\"0.315\", \"0.241\", \"0.715\", \"0.796\"]}}, \"source_image\": {\"0\": {\"faceCoordinate\": [\"0.333\", \"0.132\", \"0.778\", \"0.750\"]}}, \"pairs\": [{\"query_image\": \"0\", \"source_image\": \"0\", \"confidence\": \"-0.0759\"}]}, \"numFacesFoundQuery\": 1, \"numFacesFoundSource\": 1, \"executionMessage\": \"Faces found\", \"message\": \"success\"}";
+//        JsonParser parser = new JsonParser();
+//        JsonObject root = parser.parse(json).getAsJsonObject();
+//        int numFacesFoundQuery = root.get("numFacesFoundQuery").getAsInt();
+//        if(numFacesFoundQuery==0) {
+//        	return true;
+//        }
+////        System.out.println("numFacesFoundQuery: " + numFacesFoundQuery);
+//    	
+//    	return false;
+//    	
+//    	
+//    }
+//    
+//}
+//
